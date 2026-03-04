5a9fc71abcf5d7e7650b19004b0a5030d03f7699
gledysapriliana <gledysaprill@gmail.com>
2026-03-04 11:58:12 +0700

    Initial commit

---
+++ pilem/lib/services/api_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = 'f8bcffce4efac3df4d62e7505a4f633c';
+
+  // getAllMovies
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+  
+  // getTrendingMovies
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  // getPopularMovies
+   Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+}
